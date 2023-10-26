40b135ae8ad2ca937add859349b2fac11aa1ff13
Danny-yjs <92372710+Danny-yjs@users.noreply.github.com>
2023-10-26 18:37:39 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -74,56 +74,39 @@ def pad_sequence(sequences, batch_first=False, padding_value=0, max_len = 95):
 
 
 def get_videos():
-    video_path = []  # 存储训练集的所有图片路径
+    video_path = []  
     points_path = []
-    label = []  # 存储训练集图片对应索引信息
-    # for cla in os.listdir(path) 只遍历path第一层，不往里走
-    # path = "/home/ubuntu/nas/FB-GER/FABO/"
-    path = "/home/ubuntu/nas/FB-GER/FABO/"
-    # path_json = "/home/ubuntu/nas/FB-GER/FABO_json/"
-    path_json = "/home/ubuntu/nas/FB-GER/FABO_json/"
-    # path_S001 = "/home/ubuntu/nas/FB-GER/FABO/S001/"
-    path_S001 = "/home/ubuntu/nas/FB-GER/FABO/S001/"
-    # path_points = root + "/" + for_what + "_points"
+    label = [] 
+    # for cla in os.listdir(path) 
+   
+    path = "/__"
+    path_json = "/__"
+    path_S001 = "/__"
     emotion_class = [cla for cla in os.listdir(path_S001) if os.path.isdir(os.path.join(path_S001, cla))]
-    # !!!排序，保证顺序一致
+  
     emotion_class.sort()
-    # 生成类别名称以及对应的数字索引
     class_indices = dict((k, v) for v, k in enumerate(emotion_class))
     json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
     with open('train_class_indices.json', 'w') as train_json_file:
         train_json_file.write(json_str)
 
-    every_class_num = {}  # 存储每个类别的样本总数
-    # supported = [".jpg", ".JPG", ".png", ".PNG", ".json"]  # 支持的文件后缀类型
-    # 遍历每个文件夹下的文件
-    for S_number in os.listdir(path):  # S001,S002
-        S_path = os.path.join(path, S_number)  # D:\datasets\FABO_bjb_test\S001
+    every_class_num = {} 
+    for S_number in os.listdir(path):  
+        S_path = os.path.join(path, S_number)  
         path_points = os.path.join(path_json, S_number)
-        for cla in os.listdir(S_path):   # emotion
+        for cla in os.listdir(S_path):  
             cla_path = os.path.join(S_path, cla)
             cla_path_points = os.path.join(path_points, cla)
-            # 遍历获取supported支持的所有文件路径
+            
             video = []
             points = []
             for i in os.listdir(cla_path):   # 24_1
-                # if os.path.splitext(i)[-1] in supported:
-                #     images.append(os.path.join(path, cla, i))
-                #     i_json = os.path.splitext(i)[0] + ".json"
-                #     points.append(os.path.join(path_points, cla, i_json))
                 if os.listdir(os.path.join(cla_path, i)):
                     video.append(os.path.join(cla_path, i))
-                    # i_json = os.path.splitext(i)[0] + ".json"
                     points.append(os.path.join(cla_path_points, i))
-                    # label.append(image_class)
     
-            # images = [os.path.join(path, cla, i) for i in os.listdir(cla_path)
-            #                 if os.path.splitext(i)[-1] in supported]
-            # points = [os.path.join(path_points, cla, i) for i in os.listdir(cla_path_points)
-            #                 if os.path.splitext(i)[-1] in supported]
-            # 获取该类别对应的索引
             video_class = class_indices[cla]
-            # 记录该类别的样本数量
+            
             if cla not in every_class_num:
                 every_class_num[cla] = len(video)
             else:
@@ -137,19 +120,18 @@ def get_videos():
 
 
 def read_split_data(root: str, val_rate: float = 0.2014):
-    random.seed(0)  # 保证随机结果可复现
+    random.seed(0)  
     assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
 
-    # 遍历文件夹，一个文件夹对应一个类别
     video_path, label, points_path, every_class_num = get_videos()
     train_video_path, train_points_path, train_video_label, val_video_path, val_points_path, val_video_label = [], [], [], [], [], []
     val_class_num, train_class_num = {}, {}
-    # =======================按比例随机采样验证样本=========================
+   
     val_path = random.sample(video_path, k=int(len(video_path) * val_rate))
 
     for i in range(0, len(video_path)):
         vid_path = video_path[i]
-        if video_path[i] in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
+        if video_path[i] in val_path: 
             val_video_path.append(video_path[i])
             val_points_path.append(points_path[i])
             val_video_label.append(label[i])
@@ -157,7 +139,7 @@ def read_split_data(root: str, val_rate: float = 0.2014):
                 val_class_num[str(label[i])] = 1
             else:
                 val_class_num[str(label[i])] = val_class_num[str(label[i])] + 1
-        else:  # 否则存入训练集
+        else: 
             train_video_path.append(vid_path)
             train_video_label.append(label[i])
             train_points_path.append(points_path[i])
@@ -165,12 +147,6 @@ def read_split_data(root: str, val_rate: float = 0.2014):
                 train_class_num[str(label[i])] = 1
             else:
                 train_class_num[str(label[i])] = train_class_num[str(label[i])] + 1
-
-    # =================================================================
-
-    print("{} images for training,{} for each class".format(len(train_video_path), train_class_num))
-    print("{} images for validation, {} for each class.".format(len(val_video_path), val_class_num))
-    # print("{} images for test.".format(len(test_images_path)))
 
     return train_video_path, train_video_label, train_points_path, val_video_path, val_video_label, val_points_path
 
@@ -187,15 +163,15 @@ def plot_data_loader_image(data_loader):
     for data in data_loader:
         images, labels = data
         for i in range(plot_num):
-            # [C, H, W] -> [H, W, C]
+           
             img = images[i].numpy().transpose(1, 2, 0)
-            # 反Normalize操作
+          
             img = (img * [0.229, 0.224, 0.225] + [0.485, 0.456, 0.406]) * 255
             label = labels[i].item()
             plt.subplot(1, plot_num, i + 1)
             plt.xlabel(class_indices[str(label)])
-            plt.xticks([])  # 去掉x轴的刻度
-            plt.yticks([])  # 去掉y轴的刻度
+            plt.xticks([]) 
+            plt.yticks([])  
             plt.imshow(img.astype('uint8'))
         plt.show()
 
@@ -237,10 +213,9 @@ class LSoftmaxLoss(nn.Module):
 def train_one_epoch(model, optimizer, data_loader, device, epoch):
     model.train()
     loss_function = torch.nn.CrossEntropyLoss()
-    # l_loss_function = LSoftmaxLoss().to(device)
-    accu_loss = torch.zeros(1).to(device)  # 累计损失
-    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
-    # loss_images = torch.zeros(1).to(device)
+
+    accu_loss = torch.zeros(1).to(device)  
+    accu_num = torch.zeros(1).to(device) 
     optimizer.zero_grad()
 
     sample_num = 0
@@ -252,41 +227,27 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch):
         l_hand = data['lh_points'].to(device)
         r_hand = data['rh_points'].to(device)
         labels = data['Y'].to(device)
-        # images, labels = data    # 这里MyDataset将表情、骨骼数据与标签一起压缩，这里直接获取多个值是否可行
         sample_num += images.shape[0]
-        # pred = model((face.to(device), body.to(device), l_hand.to(device), r_hand.to(device)))
-        # N_feature, pred = model(images)
         pred, top_face_so, top_body_so = model(images, face, body, l_hand, r_hand)
         pred_classes = torch.max(pred, dim=1)[1]
         labels = torch.tensor(labels, dtype=torch.int64)
         accu_num += torch.eq(pred_classes, labels.to(device)).sum()
-        # loss_images = 0
-        # for n in range(0, N):
-        #     pred_image = N_feature[:, n, :]
-        #     loss_image = loss_function(pred_image.to(device), labels.to(device))
-        #     loss_images += loss_image
+        
         loss_two = 0
         for i in range(0, top_face_so.shape[0]):
-            # print(top_face_so[i, :].device, labels.to(device).device)
             loss_1 = loss_function(top_face_so[i, :].cuda(), labels.to(device))*6
-            # print(loss_1.device)
             loss_2 = loss_function(top_body_so[i, :].cuda(), labels.to(device))*4
-            # print(loss_2.device)
             loss_two = loss_two + loss_1 + loss_2
-            # print(loss_two.device)
+           
         loss_all = loss_function(pred, labels.to(device))*10
         loss = loss_two + loss_all
-        # loss = loss_images + loss_video
+        
         loss.backward()
-        accu_loss += loss.detach()  # 把loss取出来，相当于获取loss值
+        accu_loss += loss.detach()  
 
         data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
                                                                                accu_loss.item() / (step + 1),
                                                                                accu_num.item() / sample_num)
-
-        if not torch.isfinite(loss):
-            print('WARNING: non-finite loss, ending training ', loss)
-            sys.exit(1)
 
         optimizer.step()
         optimizer.zero_grad()
@@ -300,8 +261,8 @@ def evaluate(model, data_loader, device, epoch):
     # l_loss_function = LSoftmaxLoss().to(device)
     model.eval()
 
-    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
-    accu_loss = torch.zeros(1).to(device)  # 累计损失
+    accu_num = torch.zeros(1).to(device)  
+    accu_loss = torch.zeros(1).to(device) 
 
     sample_num = 0
     data_loader = tqdm(data_loader)
@@ -313,7 +274,7 @@ def evaluate(model, data_loader, device, epoch):
         l_hand = data['lh_points'].to(device)
         r_hand = data['rh_points'].to(device)
         labels = data['Y'].to(device)
-        # images, labels = data    # 这里MyDataset将表情、骨骼数据与标签一起压缩，这里直接获取多个值是否可行
+        
         sample_num += images.shape[0]
 
         pred, top_face_so, top_body_so = model(images, face, body, l_hand, r_hand)
@@ -323,13 +284,10 @@ def evaluate(model, data_loader, device, epoch):
 
         loss_two = 0
         for i in range(0, top_face_so.shape[0]):
-            # print(top_face_so[i, :].device, labels.to(device).device)
             loss_1 = loss_function(top_face_so[i, :].cuda(), labels.to(device))*6
-            # print(loss_1.device)
             loss_2 = loss_function(top_body_so[i, :].cuda(), labels.to(device))*4
-            # print(loss_2.device)
             loss_two = loss_two + loss_1 + loss_2
-            # print(loss_two.device)
+           
         loss_all = loss_function(pred, labels.to(device))*10
         loss = loss_two + loss_all
 
@@ -339,7 +297,5 @@ def evaluate(model, data_loader, device, epoch):
                                                                                accu_loss.item() / (step + 1),
                                                                                accu_num.item() / sample_num)
 
-        # data_loader.desc = "[test epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
-        #                                                                       accu_loss.item() / (step + 1),
-        #                                                                       accu_num.item() / sample_num)
+      
     return accu_loss.item() / (step + 1), accu_num.item() / sample_num
